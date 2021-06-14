dba9951fba6fd9679033926f972d9483ec8a3162
Giovanni Mansueto <giovanni.mansueto@dcg.com.br>
2021-06-13 21:08:13 -0300

    v 2.2

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,32 +1,22 @@
 import fetch from 'node-fetch';
 
-/*
-  openssl genrsa -out private.pem 2048
-  openssl rsa -in private.pem -pubout -out public.pem
-*/
-
 let cachedData: Buffer = null;
 
 export const getPublicKey = async () => {
   if (cachedData !== null) {
-    console.log('get from cache----');
     return cachedData;
   }
 
   if (!process.env.APP_PUBLIC_KEY) {
-    console.error('APP_PRIVATE_KEY not defined');
+    console.error('APP_PRIVATE_KEY env is not defined');
     return null;
   }
 
-  console.log('get from src----');
+  console.log('gigio teste');
+
   const response = await fetch(process.env.APP_PUBLIC_KEY);
   const pem = await response.buffer();
-  // const pem = readFileSync('./jwtRS256.key.pub');
   cachedData = pem;
-  // const iseq = bufferEq(pem, pemE);
-
-  // console.log('iseq====', iseq, pem, pemE);
-
   return pem;
 };
 
---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+// export public api from here
+// for example:
+// export * from './decorators';
+export * from './test';
+
+// import * as auth from './auth'; // import as a name
+// export { auth }; // export the name
+
+export * as auth from './auth';
+export * from './auth';
